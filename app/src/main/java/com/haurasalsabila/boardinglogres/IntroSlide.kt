3b07f8f2e8b324f0e95b185ddf4793b32c8d375a
HP <haur.salsabila17@gmail.com>
2020-07-20 16:57:18 +0700

    Initial commit

---
+++ app/src/main/java/com/haurasalsabila/boardinglogres/IntroSlide.kt
@@ -0,0 +1,10 @@
+package com.haurasalsabila.boardinglogres
+
+data class IntroSlide (
+    // Untuk menyimpan data-data yang kita buat di slide
+    // disini kita buat variable nya aja, biar kita bisa langsung panggil di adapter
+
+    val title : String,
+    val description : String,
+    val image : Int
+)
